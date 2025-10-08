2e41afc567ff23293e89eb96d659d81a74daec95
Chiron Horwood <chiron.dev8@gmail.com>
2025-10-08 15:43:43 +0100

    Add 3dbind

--- script.js
+++ script.js
@@ -100,3 +100,11 @@ function callback() {
 }
 
 resolver.resolve(options, callback);
+
+fetch('3db.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('three-db-container').innerHTML = html;
+  })
+  .catch(err => console.error('Failed to load 3db.html:', err));
+
